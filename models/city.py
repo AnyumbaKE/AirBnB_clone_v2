5a1fe6cd63c61105f29657ed8aa95cb420ed1eb9
QueenMaggie <wanjirumargaret394@gmail.com>
2023-12-20 23:23:41 -0800

    city.py

--- models/city.py
+++ models/city.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Model for City class."""
+
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, String
