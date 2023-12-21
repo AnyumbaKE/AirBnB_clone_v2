c3af293499a05aef8fc738ffd0b968545eaa661a
QueenMaggie <wanjirumargaret394@gmail.com>
2023-12-20 23:28:44 -0800

    place.py

--- models/place.py
+++ models/place.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 """Module for place class"""
+
 from sqlalchemy.ext.declarative import declarative_base
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Table, String, Integer, Float, ForeignKey
