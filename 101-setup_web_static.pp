592caf380a1b7cb2a05c6d22cd38fea07e391fc9
AnyumbaKE <esanyumba@gmail.com>
2024-01-14 06:05:57 -0800

    5. Puppet for setup

--- 101-setup_web_static.pp
+++ 101-setup_web_static.pp
@@ -1,49 +1,91 @@
 #This Setups the web servers for the deployment of web_static
-exec { '/usr/bin/env apt -y update' : }
--> package { 'nginx':
-  ensure => installed,
-}
--> file { '/data':
+
+# Nginx configuration file
+$nginx_conf = "server {
+    listen 80 default_server;
+    listen [::]:80 default_server;
+    add_header X-Served-By ${hostname};
+    root   /var/www/html;
+    index  index.html index.htm;
+
+    location /hbnb_static {
+        alias /data/web_static/current;
+        index index.html index.htm;
+    }
+
+    location /redirect_me {
+        return 301 https://youtube.com/watch?v=QH2-TGUlwu4;
+    }
+
+    error_page 404 /custom_404.html;
+    location /404 {
+      root /var/www/html;
+      internal;
+    }
+}"
+
+package { 'nginx':
+  ensure   => 'present',
+  provider => 'apt'
+} ->
+
+file { '/data':
   ensure  => 'directory'
-}
--> file { '/data/web_static':
+} ->
+
+file { '/data/web_static':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases':
+} ->
+
+file { '/data/web_static/releases':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases/test':
+} ->
+
+file { '/data/web_static/releases/test':
   ensure => 'directory'
-}
--> file { '/data/web_static/shared':
+} ->
+
+file { '/data/web_static/shared':
   ensure => 'directory'
-}
--> file { '/data/web_static/releases/test/index.html':
+} ->
+
+file { '/data/web_static/releases/test/index.html':
   ensure  => 'present',
-  content => "Testing web static deployment"
-}
--> file { '/data/web_static/current':
+  content => "Testing web static deployment\n"
+} ->
+
+file { '/data/web_static/current':
   ensure => 'link',
   target => '/data/web_static/releases/test'
-}
--> exec { 'chown -R ubuntu:ubuntu /data/':
+} ->
+
+exec { 'chown -R ubuntu:ubuntu /data/':
   path => '/usr/bin/:/usr/local/bin/:/bin/'
 }
--> file { '/var/www':
+
+file { '/var/www':
   ensure => 'directory'
-}
--> file { '/var/www/html':
+} ->
+
+file { '/var/www/html':
   ensure => 'directory'
-}
--> file { '/var/www/html/index.html':
-  ensure  => 'present'
-  content => 'Testing web static deployment'
-}
-exec { 'nginx_conf':
-  environment => ['data=\ \tlocation /hbnb_static {\n\t\talias /data/web_static/current;\n\t}\n'],
-  command     => 'sed -i "23i $data" /etc/nginx/sites-enabled/default',
-  path        => '/usr/bin:/usr/sbin:/bin:/usr/local/bin'
-}
--> service { 'nginx':
-  ensure => running,
+} ->
+
+file { '/var/www/html/index.html':
+  ensure  => 'present',
+  content => "Hello World!\n"
+} ->
+
+file { '/var/www/html/404.html':
+  ensure  => 'present',
+  content => "Ceci n'est pas une page\n"
+} ->
+
+file { '/etc/nginx/sites-available/default':
+  ensure  => 'present',
+  content => $nginx_conf
+} ->
+
+exec { 'nginx restart':
+  path => '/etc/init.d/'
 }
