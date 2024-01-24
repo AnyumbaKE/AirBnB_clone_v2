0f21787b7ef0cc93009f76a7fa14a561094392fc
Stanley Anyumba <124333266+AnyumbaKE@users.noreply.github.com>
2024-01-24 15:38:36 +0300

    servers update

--- 100-clean_web_static.py
+++ 100-clean_web_static.py
@@ -12,13 +12,7 @@ env.hosts = ["54.237.218.228", "34.204.101.100"]
 
 def do_clean(number=0):
     """Deletes out-of-date archives of the static files."""
-
-    number = int(number)
-
-    if number == 0:
-        number = 2
-    else:
-        number += 1
+    number = 1 if int(number) == 0 else int(number)
 
     archives = sorted(os.listdir("versions"))
     [archives.pop() for i in range(number)]
