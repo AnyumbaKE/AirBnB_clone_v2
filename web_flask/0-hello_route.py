8c13773fd8e4f51f5da124f57ba4c540cb349920
QueenMaggie <wanjirumargaret394@gmail.com>
2024-02-23 02:54:52 -0800

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 """A script that starts a Flask web application"""
 
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
@@ -9,9 +9,9 @@ app = Flask(__name__)
 
 
 @app.route("/", strict_slashes=False)
-def hello():
+def hello world():
     """return a given string"""
-    return render_temlate("10-hbnb_filters.html")
+    return("hbnb")
 
 
 if __name__ == '__main__':
